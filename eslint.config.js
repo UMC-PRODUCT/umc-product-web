568690bea77e07063f9ad20f44703d286e7ce5ca
Yeonjin Kim <kyj0719@gmail.com>
2025-12-29 20:57:23 +0900

    setting: husky 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,17 @@
-//  @ts-check
-
+// @ts-check
 import { tanstackConfig } from '@tanstack/eslint-config'
 
-export default [...tanstackConfig]
+export default [
+  // ✅ config 파일/빌드 파일은 린트에서 제외
+  {
+    ignores: [
+      'eslint.config.js',
+      'prettier.config.js',
+      'vite.config.ts',
+      'dist/**',
+      'node_modules/**',
+    ],
+  },
+
+  ...tanstackConfig,
+]
