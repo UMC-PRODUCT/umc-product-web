85a126aba487fff3409d3f9589a452b101b85a5f
Yeonjin Kim <kyj0719@gmail.com>
2026-01-02 01:36:13 +0900

    fix: 필요없는 코드 삭제

--- src/routes/(app)/management/route.tsx
+++ src/routes/(app)/management/route.tsx
@@ -1,16 +1,9 @@
 import { Outlet, createFileRoute } from '@tanstack/react-router'
-import Flex from '@/components/common/Flex/Flex'
 
 export const Route = createFileRoute('/(app)/management')({
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return (
-    <Flex direction="column" minHeight="100vh">
-      <Flex css={{ flex: 1 }}>
-        <Outlet />
-      </Flex>
-    </Flex>
-  )
+  return <Outlet />
 }
