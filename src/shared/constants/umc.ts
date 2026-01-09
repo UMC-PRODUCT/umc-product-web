e64636b84658a7b4533425b61fe2d1992d7425a2
Yeonjin Kim <kyj0719@gmail.com>
2026-01-09 23:58:55 +0900

    feat: constants, 공통 타입 정리

--- src/shared/constants/umc.ts
+++ src/shared/constants/umc.ts
@@ -1,6 +1,6 @@
-import type { Part } from '@/shared/types/umc/part'
+import type { AccountLevelType, PartType } from '../types/umc'
 
-export const PART: Array<Part> = [
+export const PART: Array<PartType> = [
   'Plan',
   'Design',
   'Web',
@@ -10,8 +10,24 @@ export const PART: Array<Part> = [
   'Node.js',
 ]
 
-export const FOOTER_INFO = {
+export const FOOTER_INFO: { email: string; generation: number; master: string } = {
   email: 'email.umc@example.com',
   generation: 10,
   master: '홍길동',
+}
+
+export const ACCOUNT_LEVEL: Array<{ id: number; label: AccountLevelType }> = [
+  { id: 1, label: 'ADMIN' },
+  { id: 2, label: 'MANAGER' },
+  { id: 3, label: 'USER' },
+]
+
+export const PART_ABILITY: Record<PartType, Array<string>> = {
+  Plan: ['PM', 'Figma'],
+  Design: ['UX/UI', 'Figma'],
+  Android: ['Front-End', 'Kotlin'],
+  iOS: ['Front-End', 'Swift'],
+  Web: ['Front-End', 'HTML', 'CSS', 'JavaScript'],
+  SpringBoot: ['Back-End', 'Java'],
+  'Node.js': ['Back-End', 'JavaScript'],
 }
