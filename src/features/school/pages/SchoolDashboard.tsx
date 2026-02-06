a48a4b172d98acda61a19acc23c5a3476c773cd3
Yeonjin Kim <kyj0719@gmail.com>
2026-02-06 15:18:32 +0900

    fix: 대소문자 수정

--- src/features/school/pages/SchoolDashboard.tsx
+++ src/features/school/pages/SchoolDashboard.tsx
@@ -6,10 +6,10 @@ import AsyncBoundary from '@/shared/ui/common/AsyncBoundary/AsyncBoundary'
 import { Flex } from '@/shared/ui/common/Flex'
 import SuspenseFallback from '@/shared/ui/common/SuspenseFallback/SuspenseFallback'
 
-import ApplyStatus from '../components/SchoolDashboard/ApplyStatus/ApplyStatus'
-import EvaluationStatus from '../components/SchoolDashboard/EvaluationStatus/EvaluationStatus'
-import { ProgressSteps } from '../components/SchoolDashboard/ProgressSteps/ProgressSteps'
-import ScheduleSummary from '../components/SchoolDashboard/ScheduleSummary/ScheduleSummary'
+import ApplyStatus from '../components/schoolDashboard/ApplyStatus/ApplyStatus'
+import EvaluationStatus from '../components/schoolDashboard/EvaluationStatus/EvaluationStatus'
+import { ProgressSteps } from '../components/schoolDashboard/ProgressSteps/ProgressSteps'
+import ScheduleSummary from '../components/schoolDashboard/ScheduleSummary/ScheduleSummary'
 import { useGetRecruitmentDashboard } from '../hooks/useGetRecruitingData'
 
 export const SchoolDashboardContent = () => {
